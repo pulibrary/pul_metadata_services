124861dd6d242bfcf6a1a57813696be8f6698444
Anna Headley <anna3LC@gmail.com>
2018-05-24 13:14:36 -0400

    Extract Attributes classes for record, collection

--- lib/pul_metadata_services/pulfa_record.rb
+++ lib/pul_metadata_services/pulfa_record.rb
@@ -13,18 +13,11 @@ module PulMetadataServices
     end
 
     def attributes
-      {
-        title: title,
-        language: language,
-        date_created: display_date,
-        created: normalized_date,
-        extent: extent,
-        container: container,
-        heldBy: location_code,
-        creator: collection_creators,
-        publisher: collection_creators,
-        memberOf: collections
-      }
+      if collection?
+        CollectionAttributes
+      else
+        Attributes
+      end.new(data).attributes
     end
 
     def collection?
@@ -33,91 +26,122 @@ module PulMetadataServices
 
     private
 
-    def title
-      [ [ breadcrumbs, unittitle_element.text ].reject(&:empty?).join(' - ') ].map { |s| s.gsub(/\s+/, ' ') }
-    end
+      def data
+        @data ||= reader.remove_namespaces!
+      end
 
-    # look for a component title; if not found look for a collection title
-    def unittitle_element
-      data.at_xpath("#{data_root}/did/unittitle")
-    end
+      def reader
+        @reader ||= Nokogiri::XML(source)
+      end
 
-    def language
-      text(data.at_xpath("#{data_root}/did/langmaterial/language/@langcode"))
-    end
+      class Attributes
+        attr_reader :data
+        def initialize(data)
+          @data = data
+        end
 
-    def normalized_date
-      text(data.at_xpath("#{data_root}/did/unitdate/@normal"))
-    end
+        def attributes
+          {
+            title: title,
+            language: language,
+            date_created: display_date,
+            created: normalized_date,
+            extent: extent,
+            container: container,
+            heldBy: location_code,
+            creator: collection_creators,
+            publisher: collection_creators,
+            memberOf: collections
+          }
+        end
 
-    def display_date
-      text(data.at_xpath("#{data_root}/did/unitdate"))
-    end
+        def title
+          [ [ breadcrumbs, unittitle_element.text ].reject(&:empty?).join(' - ') ].map { |s| s.gsub(/\s+/, ' ') }
+        end
 
-    def location_code
-      text(data.at_xpath("#{data_root}/did/physloc"))
-    end
+        # look for a component title; if not found look for a collection title
+        def unittitle_element
+          data.at_xpath("#{data_root}/did/unittitle")
+        end
 
-    def container
-      [ [container_parent, container_element('box'), container_element('folder')].compact.join(', ') ]
-    end
+        def language
+          text(data.at_xpath("#{data_root}/did/langmaterial/language/@langcode"))
+        end
 
-    def extent
-      text(data.at_xpath("#{data_root}/did/physdesc/extent"))
-    end
+        def normalized_date
+          text(data.at_xpath("#{data_root}/did/unitdate/@normal"))
+        end
 
-    def component_creators
-      # TODO
-    end
+        def display_date
+          text(data.at_xpath("#{data_root}/did/unitdate"))
+        end
 
-    def breadcrumbs
-      crumbs = data.xpath("#{data_root}/context/breadcrumbs/crumb")
-      crumbs.map(&:text).compact.join(' - ')
-    end
+        def location_code
+          text(data.at_xpath("#{data_root}/did/physloc"))
+        end
 
-    def collections
-      return [] if collection?
-      [{
-        title: data.at_xpath("#{data_root}/context/collectionInfo/unittitle").content,
-        identifier: data.at_xpath("#{data_root}/context/collectionInfo/unitid").content
-      }]
-    end
+        def container
+          [ [container_parent, container_element('box'), container_element('folder')].compact.join(', ') ]
+        end
 
-    def collection_creators
-      cres = data.xpath("#{data_root}/context/collectionInfo/collection-creators/*")
-      cres.map(&:content).map(&:strip)
-    end
+        def extent
+          text(data.at_xpath("#{data_root}/did/physdesc/extent"))
+        end
 
-    def collection_date
-      # TODO
-    end
+        def component_creators
+          # TODO
+        end
 
-    def data_root
-      collection? ? '/archdesc' : '/c'
-    end
+        def breadcrumbs
+          crumbs = data.xpath("#{data_root}/context/breadcrumbs/crumb")
+          crumbs.map(&:text).compact.join(' - ')
+        end
 
-    def data
-      @data ||= reader.remove_namespaces!
-    end
+        def collections
+          [{
+            title: data.at_xpath("#{data_root}/context/collectionInfo/unittitle").content,
+            identifier: data.at_xpath("#{data_root}/context/collectionInfo/unitid").content
+          }]
+        end
 
-    def reader
-      @reader ||= Nokogiri::XML(source)
-    end
+        def collection_creators
+          cres = data.xpath("#{data_root}/context/collectionInfo/collection-creators/*")
+          cres.map(&:content).map(&:strip)
+        end
 
-    def text(result)
-      [ result.text ] if result
-    end
+        def collection_date
+          # TODO
+        end
 
-    def container_parent
-      parent_id = data.at_xpath("/c/did/container/@parent")
-      return unless parent_id
-      parent = data.at_xpath("//c[@id='#{parent_id}']/did/container")
-      "#{parent.attribute('type').value.capitalize} #{parent.content}"
-    end
+        def data_root
+          '/c'
+        end
 
-    def container_element(type)
-      val = text(data.at_xpath("/c/did/container[@type='#{type}']"))
-      "#{type.capitalize} #{val.first}" if val
-    end
+        def text(result)
+          [ result.text ] if result
+        end
+
+        def container_parent
+          parent_id = data.at_xpath("/c/did/container/@parent")
+          return unless parent_id
+          parent = data.at_xpath("//c[@id='#{parent_id}']/did/container")
+          "#{parent.attribute('type').value.capitalize} #{parent.content}"
+        end
+
+        def container_element(type)
+          val = text(data.at_xpath("/c/did/container[@type='#{type}']"))
+          "#{type.capitalize} #{val.first}" if val
+        end
+      end
+
+      class CollectionAttributes < Attributes
+        def data_root
+           '/archdesc'
+        end
+
+        def collections
+          []
+        end
+      end
   end
 end
